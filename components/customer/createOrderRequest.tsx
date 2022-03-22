6025f308cc958e357067474e4bec849718fdd3f8
nonwiz0 <chanrose@pm.me>
2022-03-22 09:50:56 +0700

    working on order request

--- components/customer/createOrderRequest.tsx
+++ components/customer/createOrderRequest.tsx
@@ -1,11 +1,13 @@
 import { getFieldsValues, fetcher } from "lib/fetcher";
 import { useSWRConfig } from "swr";
+import { useState } from "react";
 
 
 
 const FormCreateOrderRequest = ({orderTypes}: {orderTypes: object[]}) => {
   const { mutate } = useSWRConfig();
 
+    const [orderItem, setOrderItem] = useState([1, 2]);
   const handleCreateOrderRequest = async event => {
     event.preventDefault();
     const formData = getFieldsValues(event, ["user", "id"])
@@ -20,14 +22,25 @@ const FormCreateOrderRequest = ({orderTypes}: {orderTypes: object[]}) => {
   return <>
         <h2> Form create order request</h2>
         <form onSubmit={handleCreateOrderRequest}>
-      
-        <input type="text" placeholder="name" name="name" />
+{orderItem.map((num) => 
+  <div className="flex flex-row" key={num}> 
+      <textarea placeholder="description" name={`description-${num}`} />
+      <input type="text" placeholder="size" name={`size-${num}`}/>
+      <input type="text" placeholder="quantity" name={`quantity-${num}`}/>
+      <input type="text" placeholder="unit_price" name={`unit_price-${num}`}/>
+      <input type="text" placeholder="amount" name={`amount-${num}`}/>
+  </div>
+
+
+
+      )}
+         <textarea placeholder="purchase_reason" name="purchase_reason" />
+
       <select name="type">
 {orderTypes.map((item, id) => (
           <option value={item.id}> {item.type} </option>
       ))}
   </select>
-        <input type="text" placeholder="name" name="name" />
 
            <input type="submit" value="Request" className="p-2 border-2 border-solid m-2" />
 

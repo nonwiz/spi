4cb2dcfd218a406dddd891daf35ace2a692fe213
dan <itskazimz@gmail.com>
2022-05-09 14:14:55 +0700

    Fix: pageheader from switch case to dict

--- components/pageHeader.tsx
+++ components/pageHeader.tsx
@@ -1,102 +1,73 @@
 import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from 'next/router'
+import departmentList from "pages/admin/departmentList";
 
 
 const PageHeader = ({ page_name }:any) => {
   const { data: session } = useSession();
 
   const router = useRouter()
-  const id = router.query.id;
-    let currentPage;
+  let path = router.pathname
 
-    //convert this into dictionary
-
-    switch (router.pathname) {
-      
-        case "/admin":
-            currentPage ="Admin"
-            break;
-        case "/admin/customerList":
-            currentPage ="Users / List"
-            break;
-        case "/admin/departmentList":
-            currentPage ="Department / List"
-            break;
-        case "/admin/userList":
-            currentPage ="User / List"
-            break;
-        case "/admin/locationList":
-            currentPage ="Location / List"
-            break;
-        case "/admin/eventLogList":
-            currentPage ="Recent"
-
-
-
-        case "/customer":
-            currentPage ="Home"
-            break;
-        case "/customer/orderRequest":
-            currentPage ="Order Requests"
-            break;
-        case "/customer/myItemes":
-            currentPage ="My Items"
-            break;
-       
-
-
-        case "/departmentHead":
-            currentPage ="Home"
-            break;
-        case "/departmentHead/orderRequest":
-            currentPage ="Order Requests"
-            break;
-        case "/departmentHead/movingRequest":
-            currentPage = "Moving Requests"
-            break;
-        case "/departmentHead/purchaseHistory":
-            currentPage ="Purchase History"
-            break;
-        case "/departmentHead/myItems":
-            currentPage ="My Items"
-            break;
-        
-
-
-        case "/purchase":
-            currentPage ="Home"
-            break;
-        case "/purchase/orderRequest":
-            currentPage ="Order Requests"
-            break;
-        case "/purchase/purchaseHistory":
-            currentPage ="Purchase History"
-            break;
-        case "/purchase/myItems":
-            currentPage ="My Items"
-            break;
-
-
-        case "/inventory":
-            currentPage ="Home"
-            break;
-        case "/inventory/locationList":
-            currentPage ="Location List"
-            break;
- 
-        case "/purchase/purchaseHistory":
-            currentPage ="Purchase History"
-            break;
-
-        case "/purchase/myItems":
-            currentPage ="My Items"
-            break;
-            
-        default:
-            currentPage ="Account Setting"
-            break;
+    const currentPage ={
+        admin: {
+            "/admin": "Admin",
+            "/admin/userList":"Users / List",
+            "/admin/departmentList":"Department / List",
+            "/admin/locationList":"Location / List",
+            "/admin/eventLogList":"Recent Activity / Logs"
+        },
+        customer:{
+            "/customer":"Home",
+            "/customer/orderRequest":"Order Requests",
+            "/customer/myItemes":"My Items"
+        },
+        department:{
+            "/departmentHead":"Home",
+            "/departmentHead/orderRequest":"Order Requests",
+            "/departmentHead/movingRequest":"Moving Requests",
+            "/departmentHead/purchaseHistory":"Purchase History",
+            "/departmentHead/myItems":"My Items",
+        },
+        purchase:{
+            "/purchase":"Home",
+            "/purchase/orderRequest":"Order Requests",
+            "/purchase/purchaseHistory":"Purchase History",
+            "/purchase/myItems":"My Items",
+        },
+        inventory:{
+            "/inventory":"Home",
+            "/inventory/locationList":"Location List",
+            "/purchase/purchaseHistory":"Purchase History",
+            "/purchase/myItems":"My Items"
+        }
     }
+
+    /**Retrieves path from currentPage based on role
+     * @param none
+     * @returns currentPage.role.['pathname']   
+     */
+    function getCurrentPage(){
+        if(router.pathname.includes("admin")){
+            return currentPage.admin[path]
+        }
+        if(router.pathname.includes("department")){
+            return currentPage.department[path]
+        }
+        if(router.pathname.includes("purchase")){
+            return currentPage.purchase[path]
+        }
+        if(router.pathname.includes("inventory")){
+            return currentPage.inventory[path]
+        }
+        if(router.pathname.includes("customer")){
+            return currentPage.customer[path]
+        }
+    }
+
+
+
   return ( 
     <div className="bg-gray-100  text-gray-700 rounded-lg px-5 py-2 flex justify-between items-center mb-4">
         {   
@@ -106,11 +77,11 @@ const PageHeader = ({ page_name }:any) => {
                 <h1 className='font-normal hidden md:block'>Inventory / Location Details</h1>
                      
             </div>:
-           <h1 className="text-xl font-bold">Dashboard / <span className='font-normal'> {currentPage}</span></h1>
+           <h1 className="text-xl font-bold">Dashboard / <span className='font-normal'> {getCurrentPage()}</span></h1>
           
          
         }
- {console.log(currentPage,"pls work ")}
+
       
                 <a className="inline-flex items-center px-4" href="#">
                     <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>

809c1d4f81c7528ce5697f08b02649b933bdf1a9
Non Wiz <nonwiz@pm.me>
2022-05-15 09:39:12 +0700

    Update: working on middleware

---
+++ pages/api/customer/order/quotation.ts
@@ -0,0 +1,19 @@
+import middleware from "middleware/middleware"
+import nextConnect from "next-connect"
+
+const handler = nextConnect()
+handler.use(middleware)
+
+handler.post(async (req, res) => {
+  console.log(req.body)
+  console.log(req.files);
+})
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default handler;
+
--- pages/customer/index.tsx
+++ pages/customer/index.tsx
@@ -11,14 +11,14 @@ import { useState } from "react";
 export default function Page() {
   const { data: session } = useSession();
   const { data, isLoading } = useCustomer();
-  const { data:info, isLoading:infoLoading } = useInfo();
+  const { data: info, isLoading: infoLoading } = useInfo();
   const [visible, setVisible] = useState(false);
   const [type, setType] = useState("none");
-  const [ office, setOffice ] = useState("")
+  const [office, setOffice] = useState("")
 
-  if (isLoading || infoLoading) return  <LoadingIcon />
+  if (isLoading || infoLoading) return <LoadingIcon />
   const pendingOrderRequests = data?.user?.location?.order_requests.filter(oreq => oreq.order_status == "Pending")
-  const createOrder = () =>{
+  const createOrder = () => {
     setVisible(true);
   }
   const closeHandler = () => {
@@ -26,36 +26,44 @@ export default function Page() {
   };
 
   return (
-      <>
+    <>
       <CreateOrderReq
-          type={type}
-          visible={visible} 
-          closeHandler={closeHandler} 
-          orderTypes={info?.order_type} 
-          locations={info?.locations}
-          quantity_unit={info?.quantity_unit}
-          office={office}
-          />
+        type={type}
+        visible={visible}
+        closeHandler={closeHandler}
+        orderTypes={info?.order_type}
+        locations={info?.locations}
+        quantity_unit={info?.quantity_unit}
+        office={office}
+      />
+      <div>
+        <h1>Test Upload file</h1>
+        <form method="post" action="/api/customer/order/quotation" enctype="multipart/form-data">
+          <input name="quotations" type="file" />
+          <button> Submit </button>
+        </form>
+
+      </div>
 
       <div className="my-8 flex flex-row gap-6 w-full items-center">
         <h1>Create Order Request: </h1>
 
-        <button onClick={() => {setOffice(""); createOrder()}} className="primary-btn"> For other location</button>
-        <button className="primary-btn" onClick={() => {setOffice(data.user.location.id); createOrder()}}>For your office </button>
+        <button onClick={() => { setOffice(""); createOrder() }} className="primary-btn"> For other location</button>
+        <button className="primary-btn" onClick={() => { setOffice(data.user.location.id); createOrder() }}>For your office </button>
       </div>
 
       <div className=" rounded-lg ">
-          <h2>Recent order Request</h2>
-          {(pendingOrderRequests?.length>0)
-            ?<OrderRequestTable orderRequest={pendingOrderRequests} /> 
-            :<EmptyState msg={"You don't have order requests"}/>}
+        <h2>Recent order Request</h2>
+        {(pendingOrderRequests?.length > 0)
+          ? <OrderRequestTable orderRequest={pendingOrderRequests} />
+          : <EmptyState msg={"You don't have order requests"} />}
       </div>
-{/*   
+      {/*   
       <div className="p-4">
         <h2> Personal Information </h2>
         <li> {data?.user?.name} | {data.user?.location ? `${data?.user?.location.short_code}` : "Not assigned"} | {data?.user?.department && data?.user?.department.name}</li>
       </div> */}
-      </>
+    </>
 
 
 
